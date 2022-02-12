05668325272130d79dd7ff073677541d623ceb9a
Matthew Duffy <duffym22@gmail.com>
2022-02-12 14:09:52 -0600

    Added constants from other classes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,7 +52,7 @@ public final class Constants {
 
     /**
      * Auto swerve wants a max acceleration.
-     * FIXME: do we have a better than the max velocity?
+     * FIXME: do we have a better option than the max velocity?
      */
     public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = MAX_VELOCITY_METERS_PER_SECOND;
 
@@ -68,58 +68,89 @@ public final class Constants {
      * Should be measured from center to center.
      */
     public static final double DRIVETRAIN_WHEELBASE_METERS = 0.5207;
+    /**
+     * The maximum angular velocity of the robot in radians per second.
+     * <p>
+     * This is a measure of how fast the robot can rotate in place.
+     */
+    // Here we calculate the theoretical maximum angular velocity. You can also
+    // replace this with a measured amount.
+    public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
+            Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
 
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 11;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 15;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 19;
 
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(139.5278); // practicebot
-    // public static final double FRONT_LEFT_MODULE_STEER_OFFSET =
-    // -Math.toRadians(142-6); //compbot
+    public static final double PRACTICE_FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(139.5278); // practicebot
+    public static final double COMPETITION_FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(142 - 6); // compbot
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 12;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 16;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 20;
 
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(144.2137); // practicebot
-    // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET =
-    // -Math.toRadians(166+180+5); //compbot
+    public static final double PRACTICE_FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(144.2137); // practicebot
+    public static final double COMPETITION_FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(166 + 180 + 5); // compbot
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 14;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 18;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 22;
 
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(226.3419); // practicebot
-    // public static final double BACK_LEFT_MODULE_STEER_OFFSET =
-    // -Math.toRadians(308); //compbot
+    public static final double PRACTICE_BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(226.3419); // practicebot
+    public static final double COMPETITION_BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(308); // compbot
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 13;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 17;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 21;
 
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(245.0254); // practicebot
-    // public static final double BACK_RIGHT_MODULE_STEER_OFFSET =
-    // -Math.toRadians(283+2); //compbot
+    public static final double PRACTICE_BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(245.0254); // practicebot
+    public static final double COMPETITION_BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(283 + 2); // compbot
 
+    // ================== SHOOTER CONSTANTS ==================
     public static final int SHOOTER_LEFT = 27;
     public static final int SHOOTER_RIGHT = 28;
     public static final double SHOOTER_SPEED = 0.5;
+    public static final double TARGET_RPM = 3200.0;
     public static final double FLYWHEEL_GEAR_RATIO = 36.0 / 24.0;
+    public static final double FLYWHEEL_TICKS_TO_ROTATIONS_COEFFICIENT = 1.0 / 2048.0 * FLYWHEEL_GEAR_RATIO;
+    public static final double FLYWHEEL_TICKS_TO_RPM_COEFFICIENT = FLYWHEEL_TICKS_TO_ROTATIONS_COEFFICIENT
+            * (1000.0 / 100.0) * (60.0);
 
+    public static final double FLYWHEEL_FEEDFORWARD_COEFFICIENT = 0.00123; // Calculated: set to 4/battery voltage
+                                                                           // output, measure speed, set this to (4 -
+                                                                           // static_cosntant) / speed
+
+    public static final double FLYWHEEL_STATIC_FRICTION_CONSTANT = 0.23; // minimum voltage to spin shooter
+
+    public static final double FLYWHEEL_ALLOWABLE_ERROR = 50.0;
+    public static final double FLYWHEEL_P = 0.1; // @todo tune this value
+    public static final double FLYWHEEL_I = 0.0;
+    public static final double FLYWHEEL_D = 0.0;
+    public static final double FLYWHEEL_CURRENT_LIMIT = 10.0;
+
+    public static final double COMPETITION_FLYWHEEL_P = 0.1; // @todo tune this value
+    public static final double COMPETITION_FLYWHEEL_I = 0.0;
+    public static final double COMPETITION_FLYWHEEL_D = 0.0;
+    public static final double COMPETITION_FLYWHEEL_CURRENT_LIMIT = 10.0;
+
+    // ================== INDEXER CONSTANTS ==================
     public static final int INDEXER_MOTOR = 24;
     public static final double FOWARDS_INDEX_SPEED = -0.5; // @todo invert motors and make these values positive after
                                                            // testing
     public static final double FOWARDS_INDEX_SPEED_SLOW = -0.15;
     public static final double REVERSE_INDEX_SPEED = 0.5;
 
+    // ================== INTAKE CONSTANTS ==================
     public static final int INTAKE = 23;
     public static final double INTAKE_FWD_SPEED = 0.5;
     public static final double INTAKE_REVERSE_SPEED = -0.5;
 
+    // ================== CENTERER CONSTANTS ==================
     public static final int CENTERER = 30;
     public static final double CENTERER_REVERSE_SPEED = -0.5;
     public static final double CENTERER_FORWARD_SPEED = 0.5;
 
+    // ================== FEEDER CONSTANTS ==================
     public static final int FEEDER = 29;
     public static final double FEEDER_FWD_SPEED = 0.75;
     public static final double FEEDER_BACKWARD_SPEED = -0.5;
@@ -132,6 +163,14 @@ public final class Constants {
     public static final double MAX_ANGULAR_VELOCITY = Math.PI * 3; // radians per second
     public static final double MAX_ANGULAR_ACCELERATION = 10.0 * 2 * Math.PI; // radians per second squared
 
+    // ================== LIMELIGHT CONSTANTS ==================
+    public static final int LEDSTATE_USE = 0;
+    public static final int LEDSTATE_OFF = 1;
+    public static final int LEDSTATE_BLINK = 2;
+    public static final int LEDSTATE_ON = 3;
+    public static final int CAMMODE_VISION = 0;
+    public static final int CAMMODE_DRIVER = 1;
+
     /**
      * Used a power limiter factor. A value between 0 and 1 with 0 as full off and 1
      * being 100% allowed.
@@ -139,4 +178,10 @@ public final class Constants {
      * For example, a value of 0.25 will cap the power to the drive trian at 25%;
      */
     public static final double TRAINING_WHEELS = 0.75;
+
+    public enum RobotDesignation {
+        NONE,
+        PRACTICE,
+        COMPETITION,
+    };
 }
