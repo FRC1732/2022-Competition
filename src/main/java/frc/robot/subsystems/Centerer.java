2b32c10fec0b04ad4c2e0f715b68fcfd92686962
Matthew Duffy <duffym22@gmail.com>
2022-02-26 16:23:49 -0600

    Refactored boolean ROBOT_IS_COMPETION to use enum instead

--- src/main/java/frc/robot/subsystems/Centerer.java
+++ src/main/java/frc/robot/subsystems/Centerer.java
@@ -11,12 +11,13 @@ import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotConfig;
+import frc.robot.Constants.RobotDesignation;
 
 public class Centerer extends SubsystemBase {
   private CANSparkMax centererMotor;
   /** Creates a new Centerer. */
   public Centerer() {
-    if (RobotConfig.ROBOT_IS_COMPETITION) {
+    if (RobotConfig.ROBOT_DESIGNATION.equals(RobotDesignation.COMPETITION)) {
       centererMotor = new CANSparkMax(Constants.CAN_CENTERER_MOTOR, MotorType.kBrushless);
       centererMotor.setInverted(true);  // 'cause of differnt motor placement
     } else {
