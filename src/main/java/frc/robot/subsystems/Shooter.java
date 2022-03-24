816e9b9bd888dee8e1ea13e4b31d1293bd52b172
dkScammer <guptaa7@muhs.edu>
2022-03-24 16:06:02 -0500

    shoot from anywhere finalized

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -199,10 +199,10 @@ public class Shooter extends SubsystemBase {
       extendHood();
     }
     if (_hoodPosition) {
-      double speed = 14.2857 * distance * distance - 221.4286 * distance + 2807.1429;
+      double speed = 4.120879 * distance * distance - 11.263736 * distance + 1741.758242;
       setTargetNearRpm(speed);
     } else {
-      double speed = 12.5 * distance * distance - 125 * distance + 2212.5 + 25;
+      double speed = 9.375 * distance * distance - 62.5 * distance + 2003.15;
       setTargetNearRpm(speed);
     }
   }
