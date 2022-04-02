89afd71e5156407ee89ca7b8360f843cef45908b
Alan Bork <alan.bork@fedex.com>
2022-04-02 15:31:00 -0500

    Chnages to Phase 2 climb
    Added a new command for Phase 2 climb that extends front arms instead of retracting back arms.
    
    Still needs limits set and PID control.

--- src/main/java/frc/robot/commands/AutoClimb/AutoClimb_Phase2.java
+++ src/main/java/frc/robot/commands/AutoClimb/AutoClimb_Phase2.java
@@ -1,7 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.AutoClimb;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -12,14 +11,8 @@ import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Intake;
 import frc.robot.commands.Climber.*;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class AutoClimb_Phase2 extends SequentialCommandGroup {
-  /** Creates a new ExtendClimberArms. */
   public AutoClimb_Phase2(Intake intakeSubsystem, Climber climberSubsystem) {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
     addCommands(
         new SequentialCommandGroup(
             new ParallelCommandGroup(
@@ -35,6 +28,5 @@ public class AutoClimb_Phase2 extends SequentialCommandGroup {
                     .withInterrupt(climberSubsystem.rightArmTwoAtRetractTarget),
                 new LeftArmTwoDown(climberSubsystem)
                     .withInterrupt(climberSubsystem.leftArmTwoAtRetractTarget))));
-
   }
 }
