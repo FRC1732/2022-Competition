3fd8aa7cf479986ab760e1170d5375fce0349db7
Alan Bork <aebork@gmail.com>
2022-04-04 12:41:48 -0500

    Added another version of Phase 2 climbing

---
+++ src/main/java/frc/robot/commands/AutoClimb/AutoClimb_Phase2_v3.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot.commands.AutoClimb;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Intake;
+import frc.robot.commands.Climber.*;
+
+public class AutoClimb_Phase2_v3 extends SequentialCommandGroup {
+  public AutoClimb_Phase2_v3(Intake intakeSubsystem, Climber climberSubsystem) {
+    addCommands(
+        new SequentialCommandGroup(
+            new ParallelCommandGroup(
+                new RightArmOneDown(climberSubsystem)
+                    .withInterrupt(climberSubsystem.rightArmOneAtRetractTarget),
+                new LeftArmOneDown(climberSubsystem)
+                    .withInterrupt(climberSubsystem.leftArmOneAtRetractTarget)),
+            new InstantCommand(() -> climberSubsystem.ArmTwoIn()),
+            new WaitCommand(.7),
+            new ParallelCommandGroup(
+                new InstantCommand(() -> intakeSubsystem.retract()),
+                new RightArmTwoDown(climberSubsystem)
+                    .withInterrupt(climberSubsystem.rightArmTwoAtRetractTarget),
+                new LeftArmTwoDown(climberSubsystem)
+                    .withInterrupt(climberSubsystem.leftArmTwoAtRetractTarget),
+                new RightArmOneUp(climberSubsystem)
+                    .withInterrupt(climberSubsystem.rightArmOneAtExtendTarget),
+                new LeftArmOneUp(climberSubsystem)
+                    .withInterrupt(climberSubsystem.leftArmOneAtExtendTarget))));
+  }
+}
