02e8002939918c96c56863c9fbd285389b91aea1
jAcKpEeLeN <45644712+InsertGenericNameHere@users.noreply.github.com>
2022-04-21 07:03:58 -0500

    collor swenjnsor constbnts

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -215,10 +215,10 @@ public final class Constants {
     public static final double LIMELIGHT_HEIGHT = 2.229;
 
     // ================== COLOR SENSOR CONSTANTS ==================
-    public static final int COLOR_SENSOR_PROX_HAS_BALL_THRESHOLD = 200;
+    public static final int COLOR_SENSOR_PROX_HAS_BALL_THRESHOLD = 175;
     public static final int COLOR_SENSOR_PROX_IDLE_THRESHOLD = 100;
 
-    public static final int COLOR_RED_BLUE_DIFFERENCE_THRESHOLD = 300;
+    public static final int COLOR_RED_BLUE_DIFFERENCE_THRESHOLD = 200;
 
     public static final int COLOR_SENSOR_RED_BALL_R_THRESHOLD = 500;
     public static final int COLOR_SENSOR_RED_BALL_G_THRESHOLD = 100;
